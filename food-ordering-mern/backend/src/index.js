b2ae99c3069744ccf3889a02d3b6f4adace00c1e
ERSHAD ALI <63920508+aliershad00@users.noreply.github.com>
2024-11-27 21:51:31 +0530

    Update index.js
    link updated

--- food-ordering-mern/backend/src/index.js
+++ food-ordering-mern/backend/src/index.js
@@ -8,8 +8,8 @@ const app = express();
 
 app.use(cors(
     {
-        origin: ["https://online-food-ordering-web-front.vercel.app/"],
-        methods: ["POST", "GET"],
+        origin: ["https://online-food-ordering-web-front.vercel.app"],
+        methods: ["POST", "GET", "PUT", "DELETE"],
         credentials: true
     }
 ));
