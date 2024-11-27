56cf9bdad2eb4345423677eac3d98896ef90e233
Ershad <aliershad056@gmail.com>
2024-11-27 21:16:58 +0530

    links are changed for deploying to vercel

--- food-ordering-mern/frontend/src/config/api.js
+++ food-ordering-mern/frontend/src/config/api.js
@@ -1,10 +1,10 @@
 import axios from 'axios';
 
-export const API_URL = "http://localhost:5454";
+export const API_URL = "https://online-food-ordering-web.vercel.app/";
 
 
 export const api = axios.create({
-  baseURL: API_URL, 
+  baseURL: API_URL,
   headers: {
     'Content-Type': 'application/json',
   },
