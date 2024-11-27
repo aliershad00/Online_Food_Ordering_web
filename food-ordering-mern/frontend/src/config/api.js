0b3f251e55975ad7f0ddc32d0450599902fe40cc
ERSHAD ALI <63920508+aliershad00@users.noreply.github.com>
2024-11-27 21:33:23 +0530

    Update api.js
    link changed

--- food-ordering-mern/frontend/src/config/api.js
+++ food-ordering-mern/frontend/src/config/api.js
@@ -1,8 +1,8 @@
 import axios from 'axios';
 
-export const API_URL = "https://online-food-ordering-web.vercel.app/";
+//export const API_URL = "https://online-food-ordering-web.vercel.app/";
 
-
+export const API_URL = "https://online-food-ordering-dynoist83-ershad-alis-projects.vercel.app/"
 export const api = axios.create({
   baseURL: API_URL,
   headers: {
